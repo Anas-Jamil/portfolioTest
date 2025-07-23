3bdf99d2cf8b9ef4d8293f6d3cdeb3096216b269
Anas Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2025-07-22 20:31:34 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline text-blue-400">
+      Hello <span className="text-2xl text-black">Bye</span> There
+    </div>
+  );
+};
+
+export default App;
